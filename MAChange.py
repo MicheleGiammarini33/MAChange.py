f94b11ba46236edd9f3e756a303bf4ed6156e24d
Michele Giammarini (He,Him) 国米克 <82205021+MicheleGiammarini33@users.noreply.github.com>
2022-06-25 18:04:02 +0200

    Update MAChange.py

--- MAChange.py
+++ MAChange.py
@@ -10,8 +10,8 @@ parser.add_option("-m","--mac",dest="new_mac",help="New MAC address")
 (options,arguments) = parser.parse_args()
 #interface =  input("interface")
 #new_mac = input("new MAC")
-interface =  options,interface
-new_mac = options,new_mac
+interface =  options.interface
+new_mac = options.new_mac
 
 print("[+]Changing MAC address for" + interface + "to" + new_mac)
 subprocess.call(["ifconfig",interface,"down"])
